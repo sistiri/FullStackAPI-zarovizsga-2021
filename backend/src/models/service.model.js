096e1f241a2c9d98f47fd10b676c7b16b02d85ef
sistiri <sistiri@yahoo.com>
2021-08-09 14:42:42 +0200

    1. Feladat

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,42 @@
-// 1. Feladat: pótold a service modellt.
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,
+        required: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
+
+// export class Service {
+//   _id: string = '';
+//   name: string = '';
+//   since: number = 0;
+//   street?: string = '';
+//   city?: string = '';
+//   country?: string = '';
+//   active?: boolean = true;
+// }
